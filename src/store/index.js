475ea5e2ff6e4231ca3b3465030b171001631b7f
Aleksanr <Aloofkot@yandex.ru>
2022-06-29 13:37:03 +0300

    configureStore toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
+//import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+//import ReduxThunk from 'redux-thunk';
 //import reducer from '../reducers';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filteres from '../reducers/filteres';
 
@@ -13,31 +14,37 @@ const stringMiddleware = ({ dispatch, getState }) => (next) => (action) => { // 
     return next(action)
 };
 
-const enhanser = (createStore) => (...args) => { // мы как бы в ручную прописываем условие и переписываем строку в объект если приходит строка
-    const store = createStore(...args);
+// const enhanser = (createStore) => (...args) => { // мы как бы в ручную прописываем условие и переписываем строку в объект если приходит строка
+//     const store = createStore(...args);
 
-    const oldDispath = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispath({
-                type: action
-            })
-        }
-        return oldDispath(action)
-    }
-    return store;
-}
+//     const oldDispath = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispath({
+//                 type: action
+//             })
+//         }
+//         return oldDispath(action)
+//     }
+//     return store;
+// }
 
-const store = createStore(
-    combineReducers({ heroes, filteres }), // комбинируем 2 редьюсера
-    compose(
-        applyMiddleware(ReduxThunk, stringMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-    // compose( // комбинируем 2 функции (или усилителей стора)// ВАЖНО УЧИТЫВАТЬ ПОСЛЕДОВАТЕЛЬНОСТЬ
-    //     enhanser,
-    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    //)
-);
+// const store = createStore(
+//     combineReducers({ heroes, filteres }), // комбинируем 2 редьюсера
+//     compose(
+//         applyMiddleware(ReduxThunk, stringMiddleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+//     // compose( // комбинируем 2 функции (или усилителей стора)// ВАЖНО УЧИТЫВАТЬ ПОСЛЕДОВАТЕЛЬНОСТЬ
+//     //     enhanser,
+//     //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     //)
+// );
+
+const store = configureStore({
+    reducer: { heroes, filteres },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),  // getDefaultMiddleware устанавливаем все дефолтные middleware из toolkit
+    devTools: process.env.NODE_ENV !== 'produktion',
+})
 
 export default store;
